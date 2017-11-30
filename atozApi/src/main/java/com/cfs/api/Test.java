3e9002b99f70813cea365741bab6df2920442908
Amit Chopra <amit.chopra@paytm.com>
2017-11-30 17:56:52 +0530

    added otp sns amazon

--- atozApi/src/main/java/com/cfs/api/Test.java
+++ atozApi/src/main/java/com/cfs/api/Test.java
@@ -1,5 +1,9 @@
 package com.cfs.api;
 
+import com.cfs.core.objects.OtpResponse;
+import com.cfs.service.IOtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +19,18 @@ import javax.ws.rs.core.Context;
  */
 @RestController
 public class Test {
+
+    @Autowired
+    private IOtpService otpService;
+
     @RequestMapping(value = "/v1/sendOtp", method = RequestMethod.GET)
     public ResponseEntity sendOtp(@RequestParam(value = "phoneNo", required = true) String phoneNo,
                                   @Context HttpServletRequest request){
-        return null;
+        OtpResponse response = otpService.sendOtp(phoneNo);
+        if (response!=null)
+            return ResponseEntity.ok(response);
+        else
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
--- atozService/src/main/java/com/cfs/service/impl/OtpServiceImpl.java
+++ atozService/src/main/java/com/cfs/service/impl/OtpServiceImpl.java
@@ -7,27 +7,49 @@ import com.amazonaws.client.AwsSyncClientParams;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.MessageAttributeValue;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.cfs.core.objects.OtpResponse;
 import com.cfs.service.IOtpService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author chopra
  * 29/11/17
  */
+@Service
 public class OtpServiceImpl implements IOtpService{
+
+    private AmazonSNS sns;
+
     @Override
     public OtpResponse sendOtp(String mobileNo) {
         if (mobileNo.length()!=10 || !mobileNo.matches(".*\\d+.*")){
-//            return null;
+            return null;
         }
 
+        OtpResponse otpResponse = new OtpResponse();
         String accesskeyId = "AKIAJLFJEQP6F64B25SA";
         String secretkey = "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";
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accesskeyId, secretkey);
 
+        String message = "Welcome to atoz";
+        mobileNo = "+91"+mobileNo;
 
+        Map<String, MessageAttributeValue> smsAttributes = new HashMap<>();
+        smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue().withStringValue("Transactional").withDataType("String"));
 
-        AmazonSNS sns = AmazonSNSClient.builder().withCredentials(new AWSStaticCredentialsProvider(awsCreds)).withRegion(Regions.AP_SOUTH_1).build();   ;//new AmazonSNSClient();
-        return null;
+        sns = AmazonSNSClient.builder().withCredentials(new AWSStaticCredentialsProvider(awsCreds)).withRegion(Regions.US_WEST_2).build();   ;//new AmazonSNSClient();
+        PublishResult result = sns.publish(new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(mobileNo).withMessageAttributes(smsAttributes));
+
+        otpResponse.setMessage("success");
+        otpResponse.setStatusCode(200);
+        return otpResponse;
     }
 }
